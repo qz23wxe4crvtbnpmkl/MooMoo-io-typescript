7f4d3d35b1402e683b01f54a0f084054be7980a1
qz23wxe4crvtbnpmkl <168938678+qz23wxe4crvtbnpmkl@users.noreply.github.com>
2024-06-30 15:16:18 -0400

    Update Math.ts

---
+++ Math.ts
@@ -0,0 +1,18 @@
+export const {
+    sin,
+    cos,
+    min,
+    max,
+    random,
+    floor,
+    ceil,
+    round,
+    PI,
+    sqrt,
+    abs,
+    pow,
+    log,
+    LN2,
+    atan2,
+    hypot
+} = Math;
